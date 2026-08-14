a051943436be937195b6ddb549fb531b6a96a69a
hasykabu-dev <hasy.kabu@gmail.com>
2026-08-14 22:59:50 +0900

    子ランニングサイト、子旅行サイト完成。 伴って、ポートフォリオプロトタイプ完成。

---
+++ trip.html/photo-data.js
@@ -0,0 +1,36 @@
+// photo=DataTransfer.js
+// 写真、国名、説明文をリストで保存
+
+const tripPhotos = [
+    {
+        image: "./photo-data/images.jpg" ,
+        country: "シンガポール",
+        description: "マーライオン"
+    },
+    {
+        image: "./photo-data/images (1).jpg" ,
+        country: "イギリス",
+        description: "大英博物館"
+    },
+    {
+        image: "./photo-data/images (2).jpg" ,
+        country: "韓国",
+        description: "明洞の屋台通り"
+    },
+    {
+        image: "./photo-data/images (3).jpg" ,
+        country: "インド",
+        description: "マハボディ寺院"
+    },
+    {
+        image: "./photo-data/images (4).jpg" ,
+        country: "ケニア",
+        description: "ファンガノ島の街"
+    },
+    // {
+    //     image: "./photo-data/images(@).jpg" ,
+    //     country: "インド",
+    //     description: "活気あふれる街並み"
+    // },
+
+]
